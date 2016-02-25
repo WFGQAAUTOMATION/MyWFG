b26b5e6cb397144466efd1c403aa0fe3fb982b5b
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2016-02-25 14:19:03 -0500

    Changed path in Run_Sanity.bat

--- Run_Sanity.bat
+++ Run_Sanity.bat
@@ -1,4 +1,4 @@
 ECHO
-call pybot --outputdir Valid_Login.html "C:\GitHub_Files\MyWFG_Model\Test_Cases\02-Homepage\MyWFG_Verify_Exec_Leadership.robot"
-::call pybot --outputdir Valid_Login.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Valid_Login.robot"
-::call pybot --outputdir Gherkin_login.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Gherkin_Login.robot"
+call pybot --outputdir Verify_Exec_Leadership_Results.html "C:\Jenkins_Files\Test_Cases\02-Homepage\MyWFG_Verify_Exec_Leadership.robot"
+::call pybot --outputdir Valid_Login_Results.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Valid_Login.robot"
+::call pybot --outputdir Gherkin_Login_Results.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Gherkin_Login.robot"
