b141aa1989cd83c35bb02b7f202b7e33cc238068
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2016-02-25 14:57:18 -0500

    Added bat file

---
+++ Run_Sanity.bat
@@ -0,0 +1,5 @@
+ECHO
+call pybot --outputdir Verify_Exec_Leadership_Results.html "C:\Jenkins_Files\Test_Cases\09-LifeLine_Footer\LifeLine_Archive_Open_Close.robot"
+call pybot --outputdir Verify_Exec_Leadership_Results.html "C:\Jenkins_Files\Test_Cases\02-Homepage\MyWFG_Verify_Exec_Leadership.robot"
+::call pybot --outputdir Valid_Login_Results.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Valid_Login.robot"
+::call pybot --outputdir Gherkin_Login_Results.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Gherkin_Login.robot"
