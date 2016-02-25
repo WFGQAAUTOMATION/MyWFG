e3693d183d7c9523d811d582caff7673856b62b9
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2016-02-25 14:12:05 -0500

    Added Homepage check to Run_Sanity.bat

--- Run_Sanity.bat
+++ Run_Sanity.bat
@@ -1,3 +1,4 @@
 ECHO
-call pybot --outputdir Valid_Login.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Valid_Login.robot"
-call pybot --outputdir Gherkin_login.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Gherkin_Login.robot"
+call pybot --outputdir Valid_Login.html "C:\GitHub_Files\MyWFG_Model\Test_Cases\02-Homepage\MyWFG_Verify_Exec_Leadership.robot"
+::call pybot --outputdir Valid_Login.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Valid_Login.robot"
+::call pybot --outputdir Gherkin_login.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Gherkin_Login.robot"
