98e5b924cd203c0eff4fa705b39dbf993f19d3a7
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2016-02-25 17:06:25 -0500

    Update Run_Sanity.bat

--- Run_Sanity.bat
+++ Run_Sanity.bat
@@ -1,5 +1,5 @@
 ECHO
-call pybot --outputdir Verify_Exec_Leadership_Results.html "C:\Jenkins_Files\Test_Cases\09-LifeLine_Footer\LifeLine_Archive_Open_Close.robot"
-call pybot --outputdir Verify_Exec_Leadership_Results.html "C:\Jenkins_Files\Test_Cases\02-Homepage\MyWFG_Verify_Exec_Leadership.robot"
+call pybot --outputdir Gherkin_Login_Results.html "C:\Jenkins_Files\Test_Cases\01-Login\*.robot"
+call pybot --outputdir Verify_Exec_Leadership_Results.html "C:\Jenkins_Files\Test_Cases\02-Homepage\*.robot"
+::call pybot --outputdir Verify_Exec_Leadership_Results.html "C:\Jenkins_Files\Test_Cases\09-LifeLine_Footer\LifeLine_Dates_Verification_New_IAR_Training_Course.robot"
 ::call pybot --outputdir Valid_Login_Results.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Valid_Login.robot"
-::call pybot --outputdir Gherkin_Login_Results.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Gherkin_Login.robot"
