3a0691b073310dc6caacd5d7506fcbb5fc903340
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2016-02-25 13:50:50 -0500

    Update Run_Sanity.bat

--- Run_Sanity.bat
+++ Run_Sanity.bat
@@ -1,2 +1,3 @@
 ECHO
-pybot "C:\Jenkins_Files\Test_Cases\01-Login\*.robot"
+call pybot --outputdir Valid_Login.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Valid_Login.robot"
+call pybot --outputdir Gherkin_login.html "C:\Jenkins_Files\Test_Cases\01-Login\MyWFG_Gherkin_Login.robot"
