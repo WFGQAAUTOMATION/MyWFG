31e802b3be0ecddde98e92ab15c12c6d63fbc316
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2016-02-26 15:20:42 -0500

    Update Run_Sanity.bat

--- Run_Sanity.bat
+++ Run_Sanity.bat
@@ -3,3 +3,4 @@ call pybot --outputdir Gherkin_Login_Results.html "C:\Jenkins_Files\Test_Cases\0
 call pybot --outputdir Verify_Exec_Leadership_Results.html "C:\Jenkins_Files\Test_Cases\02-Homepage\*.robot"
 call pybot --outputdir Dashboard_Results.html "C:\GitHub_Files\MyWFG_Model\Test_Cases\03-Dashboard\*.robot"
 call pybot --outputdir My_Preferences_Results.html "C:\Jenkins_Files\Test_Cases\08-Profile\02-My_Preferences\*.robot"
+call pybot --outputdir Lifeline_Results.html "C:\Jenkins_Files\Test_Cases\09-LifeLine_Footer\*.robot"
