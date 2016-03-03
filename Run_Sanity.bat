135f7fd601b10847cac5fbf33a72951bd043df9a
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2016-03-03 15:33:38 -0500

    Update Run_Sanity.bat

--- Run_Sanity.bat
+++ Run_Sanity.bat
@@ -1,6 +1,6 @@
 ECHO
 call pybot --outputdir Login_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\01-Login\*.robot"
-::call pybot --outputdir Verify_Home_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\02-Homepage\*.robot"
-::call pybot --outputdir Dashboard_Results_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\03-Dashboard\*.robot"
-::call pybot --outputdir My_Preferences_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\08-Profile\02-My_Preferences\*.robot"
-::call pybot --outputdir Lifeline_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\09-LifeLine_Footer\*.robot"
+call pybot --outputdir Verify_Home_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\02-Homepage\*.robot"
+call pybot --outputdir Dashboard_Results_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\03-Dashboard\*.robot"
+call pybot --outputdir My_Preferences_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\08-Profile\02-My_Preferences\*.robot"
+call pybot --outputdir Lifeline_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\09-LifeLine_Footer\*.robot"
