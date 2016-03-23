dbec9e74a1051d5fb7e1511b7f8dc3692abca9e4
WFGQAAUTOMATION <tim.coffey@transamerica.com>
2016-03-23 15:51:20 -0400

    Update Run_Sanity.bat

--- Run_Sanity.bat
+++ Run_Sanity.bat
@@ -1,7 +1,7 @@
 ECHO
 set Pathname="C:\Jenkins_Files"
 cd /d %Pathname%
-call pybot --outputdir Login_Log --variablefile ..\Model_Variables.py "..\Test_Cases\01-Login\*.robot"
+call pybot --outputdir Login_Log --variablefile \Model_Variables.py "\Test_Cases\01-Login\*.robot"
 ::call pybot --outputdir Verify_Home_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\02-Homepage\*.robot"
 ::call pybot --outputdir Dashboard_Results_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\03-Dashboard\*.robot"
 ::call pybot --outputdir My_Preferences_Log --variablefile C:\Jenkins_Files\Variable_Files\Model_Variables.py "C:\Jenkins_Files\Test_Cases\08-Profile\02-My_Preferences\*.robot"
